914035b9cc6a9d04057d1def1679d6c9903370f0
ChaoZengCN <80757590@qq.com>
2021-04-19 19:11:40 +0800

    Add files via upload

---
+++ Uat_Subscribe.py
@@ -0,0 +1,92 @@
+import psycopg2,time
+from Config import ConfigSql,ConfigDatabase
+
+
+class config:
+    configDatabase = ConfigDatabase()
+    configSql = ConfigSql()
+
+class uatSubscribe(config):
+    '''封装方法'''
+    
+    #连接数据库，执行sql，关闭连接
+    def dataCenterExecute(self,center,sql):
+        #获取数据库信息
+        self.database_Data = self.configDatabase.operateYaml('UAT')
+        db = None
+        #连接数据库
+        try:
+            db = psycopg2.connect(database=center,user=self.database_Data['username'], password=self.database_Data['password'], host=self.database_Data['host'], port=self.database_Data['port'])
+            db.autocommit = True
+            cur = db.cursor()
+            print(sql)
+            cur.execute(sql)
+            self.result = cur.fetchall()
+            cur.close()
+            if self.result is not None:
+                print(self.result)
+                return self.result
+        except (Exception, psycopg2.DatabaseError) as e:
+            print(e)
+        finally:
+            if db is not None:
+                db.close()
+    
+    #清空数据中心所有的表
+    def clearTable(self):
+        # select tablename from pg_tables where tablename like 'lc%' or tablename like 'tc%' or tablename like  'stc%' or tablename like  'ctc%';   查询出所有的表
+        # TRUNCATE tablename RESTART IDENTITY;    清空表数据并还原 sequence
+        self.result = self.dataCenterExecute('datacenter',"select tablename from pg_tables where tablename like 'lc%' or tablename like 'tc%' or tablename like  'stc%' or tablename like  'ctc%';")
+        try:
+            print('\n--------------开始删除表数据----------')
+            for name in range(0,len(self.result)):
+                self.tablename = str(self.result[name]).replace('\'','').replace(',','').replace('(','').replace(')','')
+                self.sql = "TRUNCATE {} RESTART IDENTITY;".format(self.tablename)
+                self.dataCenterExecute('datacenter',self.sql)
+                print('清除 %s 表数据!' % self.tablename)
+            print('--------------删除表数据结束----------\n')
+        except Exception as e:
+            print(e)
+
+    # 根据步骤连接数据库并执行
+    def part(self,part):
+        try:
+            self.data = config.configSql.operateYaml(part)
+            for num in range(0,len(self.data)):
+                self.center = self.data[num]['center']
+                self.sql = self.data[num]['sql']
+                for i in range(0,len(self.sql)):
+                    time.sleep(1)
+                    self.dataCenterExecute(self.center,self.sql[i])
+        except Exception as e:
+            print(e)
+    
+
+
+class main(uatSubscribe):
+    '''执行逻辑'''
+    uatSubscribe = uatSubscribe()
+    # 第一步，删除数据中心所有的订阅,之后清空所有的表
+    # 第一步，删除数据中心所有表的数据
+    print('==================================================')
+    print('=============测试环境重新订阅开始===================')
+    print('==================================================')
+    uatSubscribe.part('part1')
+    uatSubscribe.clearTable()
+    # 第二步，删除各个中心所有的插槽，并重建
+    uatSubscribe.part('part2')
+    # 第三步，重建数据中心所有的订阅
+    uatSubscribe.part('part3')
+    print('==================================================')
+    print('=============测试环境重新订阅结束===================')
+    print('==================================================')
+
+
+
+if __name__ == "__main__":
+    main()
+    # uatSubscribe = uatSubscribe()
+    # uatSubscribe.part('part1')
+    # uatSubscribe.clearTable()
+    # uatSubscribe.part('part2')
+    # uatSubscribe.part('part3')
